3e9aadabd1184703571277f73cb8151eada14223
viv-mel <viviana.melian@yahoo.com>
2025-02-28 11:23:53 +0100

    Configuring versioned API

--- UsersArbolusAPI/Controllers/UsersController.cs
+++ UsersArbolusAPI/Controllers/UsersController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
 using UsersArbolusAPI.Models;
 using UsersArbolusAPI.Services;
 
 namespace UsersArbolusAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [ApiVersion(1)]
+    [Route("api/v{v:apiVersion}/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
@@ -15,10 +17,22 @@ namespace UsersArbolusAPI.Controllers
             this.usersService = usersService;
         }
 
-        [HttpGet(Name = "GetUsers")]
-        public async Task<UserList> Get()
+        [HttpGet("get-users")]
+        public async Task<UserList> Get([FromQuery] int page = 1, CancellationToken cancellationToken = default)
         {
-            return await usersService.GetUsers();
+            return await usersService.GetUsers(page, cancellationToken);
+        }
+
+        [HttpGet("get-older-user")]
+        public async Task<User?> GetOlderUser([FromQuery] int page = 1, CancellationToken cancellationToken = default)
+        {
+            return await usersService.GetOlderUser(page, cancellationToken);
+        }
+
+        [HttpGet("get-users-by-gender/{gender}")]
+        public async Task<IEnumerable<User>> GetUsersByGender([FromRoute] string gender, [FromQuery] int page = 1, CancellationToken cancellationToken = default)
+        {
+            return await usersService.GetUsersByGender(gender, page, cancellationToken);
         }
     }
 }
--- UsersArbolusAPI/Program.cs
+++ UsersArbolusAPI/Program.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using Polly;
 using UsersArbolusAPI.Options;
 using UsersArbolusAPI.Services;
@@ -12,6 +13,23 @@ var arbolusApiOptions = builder.Configuration.GetSection(ArbolusApiOptions.Secti
 builder.Services.Configure<ArbolusApiOptions>(arbolusApiOptions);
 
 // Add services to the container.
+
+// Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version"));
+})
+.AddMvc()
+.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
+});
 
 // Configuring HttpClients
 var arbolusBaseAddress = arbolusApiOptions.GetValue<string>(nameof(ArbolusApiOptions.ApiBaseUrl));
